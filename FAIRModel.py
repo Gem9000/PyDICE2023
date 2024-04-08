a1a18782b6ae99d3d98265bd5848d4a836486004
Gem9000 <gemoraites@gmail.com>
2024-04-08 15:03:56 -0400

    Did a dry test run of the model with the algorithm. Now I'm trying to dynamically find the root, but it's currently not working.

--- FAIRModel.py
+++ FAIRModel.py
@@ -8,7 +8,7 @@ import numpy as np
 import math
 import csv
 
-from scipy.optimize import fsolve
+from scipy.optimize import root_scalar
 
 '''
 ** Equals old FAIR with recalibrated parameters for revised F2xco2 and Millar model.
@@ -101,7 +101,7 @@ class FAIRParams():
         #Num times should be 81
         #time increment should be 5 years
         self._num_times = num_times
-        self._time_increment = np.arrange(0,self.num_times+1)
+        self._time_increment = np.arange(0,self._num_times+1)
 
         '''
         EQUATIONS
@@ -120,42 +120,42 @@ class FAIRParams():
         '''
         
         #Creating Size arrays so we can index from 1 instead of zero 
-        self._res0lom = np.zeros(num_times+1)
-        self._res1lom =np.zeros(num_times+1)
-        self._res2lom = np.zeros(num_times+1)
-        self._res3lom = np.zeros(num_times+1)
-        self._mmat = np.zeros(num_times+1)
-        self._cacceq = np.zeros(num_times+1)
-        self._force = np.zeros(num_times+1)
-        self._tbox1eq = np.zeros(num_times+1)
-        self._tbox2eq = np.zeros(num_times+1)
-        self._tatmeq = np.zeros(num_times+1)
-        self._irfeqlhs = np.zeros(num_times+1)
-        self._irfeqrhs = np.zeros(num_times+1)
-        self._alpha_t = np.zeros(num_times+1)
+        self._res0lom = np.zeros(num_times+2)
+        self._res1lom =np.zeros(num_times+2)
+        self._res2lom = np.zeros(num_times+2)
+        self._res3lom = np.zeros(num_times+2)
+        self._mmat = np.zeros(num_times+2)
+        self._cacceq = np.zeros(num_times+2)
+        self._force = np.zeros(num_times+2)
+        self._tbox1eq = np.zeros(num_times+2)
+        self._tbox2eq = np.zeros(num_times+2)
+        self._tatmeq = np.zeros(num_times+2)
+        self._irfeqlhs = np.zeros(num_times+2)
+        self._irfeqrhs = np.zeros(num_times+2)
+        self._alpha_t = np.zeros(num_times+2)
 
         #Filler for the model
-        self._F_Misc = np.zeros(num_times+1)
+        self._F_Misc = np.zeros(num_times+2)
 
         #Fillers for equations that are in the DICE model equation
-        self._eco2 = np.zeros(num_times+1)
-        self._F_GHGabate = np.zeros(num_times+1)
-        self._CCATOT = np.zeros(num_times+1)
+        self._eco2 = np.zeros(num_times+2)
+        self._F_GHGabate = np.zeros(num_times+2)
+        self._CCATOT = np.zeros(num_times+2)
         
         #Timestep counter
-        self._t = np.arange(0,num_times+1)
+        self._t = np.arange(0,num_times+2)
 
         #Initial conditions
-        self._mateq[1] = self._mat0
+        self._mmat[1] = self._mat0
         self._tatmeq[1] = self._tatm0
         self._res0lom[1] = self._res00
         self._res1lom[1] = self._res10
         self._res2lom[1] = self._res20
         self._res3lom[1] = self._res30
         self._tbox1eq[1] = self._tbox10
-        self._tbox2eq[1] = self._tbox2eq
+        self._tbox2eq[1] = self._tbox20
     
-        for i in range(2, self._num_times + 1):
+        for i in range(1, self._num_times + 1):
 
             #Solve for alpha(t) in each time period 
             self._alpha_t[i+1] = self.solve_alpha(i)
@@ -180,10 +180,14 @@ class FAIRParams():
                                     (1 - math.exp(-self._tstep / (self._tau3 * self.solve_alpha(i+1)))) + 
                                     self._res3lom[i] * math.exp(-self._tstep / (self._tau3 * self.solve_alpha(i+1))))
 
-            self._mmat[i+1] = self._mateq + self._res0lom[i+1] + self._res1lom[i+1] + self._res2lom[i+1] + self._res3lom[i+1]
+            calculated_mmat = self._mateq + self._res0lom[i+1] + self._res1lom[i+1] + self._res2lom[i+1] + self._res3lom[i+1]
+            if calculated_mmat < 20:
+                self._mmat[i+1] = 20
+            else:
+                self._mmat[i+1] = calculated_mmat
                 
-            self._force[i] = (self._fco22x * (math.log((self._mmat[i]/self._mateq))/math.log(2)) 
-                                + self._F_Misc[i] + self._F_GHGabate[i])
+            self._force[i] = (self._fco22x * ((math.log((self._mmat[i]+1e-9/self._mateq))/math.log(2)) 
+                                + self._F_Misc[i] + self._F_GHGabate[i]))
 
             self._tbox1eq[i+1] = (self._tbox1eq[i] *
                                     math.exp(self._tstep/self._d1) + self._teq1 *
@@ -193,31 +197,30 @@ class FAIRParams():
                                     math.exp(self._tstep/self._d2) + self._teq2 *
                                     self._force[i+1] * (1-math.exp(self._tstep/self._d2)))
 
-            self._tatmeq[i+1] = self._tbox1eq[i+1] + self._tbox2eq[i+1]
+            self._tatmeq[i+1] = np.clip(self._tbox1eq[i+1] + self._tbox2eq[i+1], 0.5, 20)
 
-            self._cacceq[i] = (self._CCATOT[i] - (self._mateq[i] - self._mateq))
+            self._cacceq[i] = (self._CCATOT[i] - (self._mmat[i] - self._mateq))
 
         #Adding in additional code for creating a CSV with the fair model 
         #Equation values
 
         if 1==1:
 
-            f = open("./results/parameters.csv" , mode = "w", newline='')
+            f = open("./results/FAIR_Model.csv" , mode = "w", newline='')
             writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
                 
             header = []
-            header.append("PERIOD")
-            header.append("VARPCC")
-            header.append("RPRECAUT")
-            header.append("RR1")
-            header.append("RR")
-            header.append("L")
-            header.append("GA")
-            header.append("AL")
-            header.append("CPRICEBASE")
-            header.append("PBACKTIME")
-            header.append("GSIG")
-            header.append("SIGMA")
+            header.append("Alpha(t)")
+            header.append("Reservoir 0")
+            header.append("Reservoir 1")
+            header.append("Reservoir 2")
+            header.append("Reservoir 3")
+            header.append("Atmospheric Concentration Equation")
+            header.append("Accumulated Carbon in Sinks")
+            header.append("Temp-Climate Equation for Atmosphere")
+            header.append("Temp. box 1")
+            header.append("Temp. box 2")
+            header.append("Radiative Forcing")
             writer.writerow(header)
 
             num_rows = self._num_times + 1
@@ -225,62 +228,62 @@ class FAIRParams():
             for i in range(0, num_rows):
                 row = []
                 row.append(i)
-                row.append(self._varpcc[i])
-                row.append(self._rprecaut[i])
-                row.append(self._RR1[i])
-                row.append(self._rr[i])
-                row.append(self._l[i])
-                row.append(self._gA[i])
-                row.append(self._al[i])
-                row.append(self._cpricebase[i])
-                row.append(self._pbacktime[i])
-                row.append(self._gsig[i])
-                row.append(self._sigma[i])
+                row.append(self._alpha_t[i])
+                row.append(self._res0lom[i])
+                row.append(self._res1lom[i])
+                row.append(self._res2lom[i])
+                row.append(self._res3lom[i])
+                row.append(self._mmat[i])
+                row.append(self._cacceq[i])
+                row.append(self._tatmeq[i])
+                row.append(self._tbox1eq[i])
+                row.append(self._tbox2eq[i])
+                row.append(self._force[i])
 
                 writer.writerow(row)
 
-                f.close()
+            f.close()
 
-        elif 1==2:
-            print("Variance of per capita consumption:", self._varpcc)
-            print("Precationary rate of return:",self._rprecaut)
-            print("STP factor without precationary factor:",self._RR1)
-            print("STP factor with precationary factor:",self._rr)
-            print("Labour:", self._l) # CHECKED OK
-            print("Growth rate of productivity", self._gA) # CHECKED OK
-            print("Productivity", self._al) # CHECKED OK
-            print("Carbon price based case", self._cpricebase) # AGREES WITH NORDHAUS UNTIL 2235 ??
-            print("Backstop price", self._pbacktime) # CHECKED OK
-            print("Change in sigma", self._gsig) # CHECKED OK
-            print("CO2 output ratio:", self._sigma) # CHECKED OK
-            print("Long run savings rate", self._optlrsav) # CHECKED OK
+        elif 1==2: 
+            print("Variance of per capita consumption:", self._alpha_t) # Need to check
+            print("Precationary rate of return:",self._res0lom) # Need to check
+            print("STP factor without precationary factor:",self._res1lom) # Need to check
+            print("STP factor with precationary factor:",self._res2lom) # Need to check
+            print("Labour:", self._res3lom) # Need to check
+            print("Growth rate of productivity", self._mmat) # Need to check
+            print("Productivity", self._cacceq) # Need to check
+            print("Carbon price based case", self._tatmeq) # Need to check
+            print("Backstop price", self._tbox1eq) # Need to check
+            print("Change in sigma", self._tbox2eq) # Need to check
+            print("CO2 output ratio:", self._force) # Need to check
 
         else:
             print("SOME CHECKING TO BE DONE")
+    
 
-
-
-     #Create a function to implicitly solve the 
-            
-    #This solves for the irfeqlhs and irfeqrhs equations
+                    #This solves for the irfeqlhs and irfeqrhs equations
     def solve_alpha(self, t):
         # Define the equation for IRFt(t) using the given parameters and functions
-        def equation(alpha):
-            return ((alpha * self._emshare0 * self._tau0 * (1 - np.exp(-100 / (alpha * self._tau0)))) +
-                    (alpha * self._emshare1 * self._tau1 * (1 - np.exp(-100 / (alpha * self._tau1)))) +
-                    (alpha * self._emshare2 * self._tau2 * (1 - np.exp(-100 / (alpha * self._tau2)))) +
-                    (alpha * self._emshare3 * self._tau3 * (1 - np.exp(-100 / (alpha * self._tau3))))) - \
-                    (self._irf0 + self._irC * self._cacceq(t) + self._irT * self._tatmeq(t))
+        def equation(alpha, *args):
+            return (
+                (alpha * self._emshare0 * self._tau0 * (1 - np.exp(-100 / (alpha * self._tau0)))) +
+                (alpha * self._emshare1 * self._tau1 * (1 - np.exp(-100 / (alpha * self._tau1)))) +
+                (alpha * self._emshare2 * self._tau2 * (1 - np.exp(-100 / (alpha * self._tau2)))) +
+                (alpha * self._emshare3 * self._tau3 * (1 - np.exp(-100 / (alpha * self._tau3)))) - 
+                (self._irf0 + self._irC * self._cacceq[t] + self._irT * self._tatmeq[t])
+            )
 
-        # Use fsolve to solve for alpha
+        # Use root_scalar to solve for alpha
         alpha_initial_guess = 1.0
-        alpha_solution = fsolve(equation, alpha_initial_guess, bounds = (0.1, 100))
-        return alpha_solution
+        alpha_solution = root_scalar(equation, alpha_initial_guess, bracket=(0.1, 100),method= 'brenth')
+        return alpha_solution.root
 
     def runModel(self):
         pass    
 
 print("Success")
 
+fair_params = FAIRParams(num_times=81, tstep= 5)
 
+fair_params.runModel()
 
